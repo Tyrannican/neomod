93bf99882494cf2059d4c1df2318fcf1e58240c0
Graham Keenan <graham.keenan@outlook.com>
2024-01-09 19:50:33 +0000

    starting fresh, curseforge sucks

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,10 @@ use anyhow::{anyhow, Result};
 use serde::{Deserialize, Serialize};
 use tokio::fs;
 
-type Manifest = Vec<Addon>;
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Addon {
-    pub name: String,
-    pub author: String,
-    pub description: String,
-    pub versions: Vec<String>,
-    pub categories: Vec<String>,
-    pub source: AddonSource,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-pub struct AddonSource {
-    pub git: Option<String>,
-    pub zip: String,
-}
-
 #[derive(Default, Debug, Serialize, Deserialize)]
 pub struct Neomod {
     pub store: PathBuf,
     pub wow_loc: PathBuf,
-    pub manifest: Manifest,
-    pub installed: Manifest,
 }
 
 pub async fn init_neomod() -> Result<Neomod> {
@@ -37,16 +17,9 @@ pub async fn init_neomod() -> Result<Neomod> {
         let mut neomod = Neomod::default();
         neomod.store = target_dir.clone();
 
-        let manifest_str = include_str!("../manifest.json");
-        let mut manifest = target_dir.clone();
-        manifest.push("manifest.json");
-
         if !target_dir.exists() {
             fs::create_dir(&target_dir).await?;
-            fs::write(&manifest, manifest_str.as_bytes()).await?;
         }
-
-        neomod.manifest = serde_json::from_str(&manifest_str)?;
 
         return Ok(neomod);
     }
@@ -56,7 +29,5 @@ pub async fn init_neomod() -> Result<Neomod> {
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let neomod = init_neomod().await?;
-    println!("App: {neomod:?}");
     Ok(())
 }
